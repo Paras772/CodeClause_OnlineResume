959527795a2afb4546821ce11712f4960c50b2a8
Paras772 <123758267+Paras772@users.noreply.github.com>
2023-07-26 21:19:40 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,155 @@
+const profileData = {
+  title: "Resume",
+  name: "Paras .",
+  sub_title: "Intern @CodeClause",
+  logoURL: "assets/images/dp.jpg",
+  about: {
+    intro: `I am an innovative and passionate developer with a primary interest in <strong>JavaScript</strong>
+     and its applications. Experienced in designing and implementing sophisticated UI/UX with
+      Progressive Web Application standards and application life-cycle management.`,
+    contact: {
+      email: "paraskumar74569@gmail.com",
+      phone: "+91-9548492266",
+      address: "Dehradun, IN"
+    }
+  },
+  links: [
+    //{ title: "Codepen", src: "https://codepen.io/imvpn22" },
+    { title: "Github", src: "https://github.com/paras772" },
+    { title: "LinkedIn", src: "https://www.linkedin.com/in/paras-998a6b246/" }
+    // { title: 'Twitter', src: 'https://twitter.com/imvpn22' }
+    // { title: 'HackerEarth', src: 'https://www.hackerearth.com/@imvpn22' },
+  ],
+  education: [
+    {
+      alma: "UPES Dehradun",
+      duration: "2021 - 2025",
+      std: "B.Tech. (Computer Science & Engineering)",
+      score: "80.04%(Upto 4th Sem)"
+    },
+    {
+      alma: "Kendriya Vidyalaya IMA Dehradun",
+      duration: "2020 - 2021",
+      std: "Class XI-XII (PCM)",
+      score: "94.20%"
+    },
+    {
+      alma: "Dehradun Public School",
+      duration: "2018-19",
+      std: "Class IX - X (PCM)",
+      score: "91.20%"
+    }
+  ],
+  skills: [
+    {
+      category: "Programming Languages",
+      topics: ["C++", "C", "Html","JavaScript","Css","Java"]
+    },
+    {
+      category: "Frameworks / Libraries",
+      topics: [
+        "Amazon Web Services",
+        "Shell",
+        "Linux",
+        "Google Cloud",
+        "DevOps",
+        "Microsoft Azure",
+      ]
+    }
+  ],
+  projects: [
+    {
+      title: "Cloud Based E-commerce Platform",
+      duration: "April-May 2023",
+      link: "https://github.com/Ankitraj822612/Saas-Project",
+      desc: `A web app to list, view and buy/sell products.
+      The app is based on AWS Backed Services & reliable architecture.
+      This project was a part of ou 4th semester Cloud Computing Architecture course.
+      Developed using <strong>AWS , Html</strong>,<strong>CSS</strong> and <strong>JavaScript</strong>
+      with Progressive Web-Application standards including full-responsiveness,
+      offline support, and improved accessibility.`
+    },
+    {
+      title: "To Do list webapp",
+      duration: "July 2023",
+      link: "",
+      desc: `A simple To-do list <strong>web-app for listing all the task to be done before the </strong>
+      completeion date 
+      It also has features to delete the added tasks.
+      Developed using <strong> CSS, javascript</strong> 
+      <strong>with HTML</strong> <strong></strong><strong></strong>.`
+    }
+  ],
+  experiences: [
+    {
+      organization: "Cloud Counselage Private limited",
+      title: `Cloud Computing Intern`,
+      date: "July-September 2023",
+      details: [
+        `Worked on the development of Cloud Computing Applications with help of 
+        <strong> Amazon Web Services </strong> with <strong> Azure</strong> and <strong>Google Cloud </strong> <strong></strong>
+        `,
+        //`Created a customized Content Management System for MPL's website, to update content and images on the fly for different pages.`
+      ]
+    },
+    {
+      organization: "Oasis-Infobyte",
+      title: `Web Developement & Design Intern`,
+      //desc: `Project Engineer`,
+      date: "July 2023 - August 2023",
+      details: [
+        `Contributed to the development of a <strong> Calculator,
+        To-do list webapp and tribute page </strong> 
+         using <strong>Javascript </strong> with <strong>CSS</strong>,
+        <strong> HTML </strong><strong></strong> with help of Sublime-Text editor & VSCode.`,
+       // `Resolved UI/UX issues by refactoring React components and re-structuring CSS/SCSS of an internal project with the codebase of ReactJS-Redux.`
+      ]
+    },
+    {
+      organization: "CodeClause",
+      title: "Web-Development Intern",
+      desc: `<strong>Web-Dev intern</strong> at  <a target='_blank' rel='noreferrer' href='https://www.venuemonk.com/'>
+      <strong> CodeClause </strong> </a> (<a target='_blank' rel='noreferrer' href='https://drive.google.com/open?id=1ch_9x9jCp_TJwKY43FG-pCr_zV9H2iun'> https://goo.gl/Uz1gaf</a>)`,
+      date: "July 2023",
+      details: [
+        `Designed and developed a <strong> Basic Online Resume & To-Do webapp  </strong> using <strong>HTML CSS</strong> and
+        <strong>Javascript </strong> with Sublime Test-Editor & VSCode.`
+      ]
+    },
+    {
+      organization: "Khushiyaan Foyundation(NGO)",
+      title: "Science/Maths Tutor",
+      desc: `<strong> Teacher </strong> at <a target='_blank' rel='noreferrer' href='https://hasura.io/'>
+      <strong> Khushiyaan Foundation </strong> </a> (<a target='_blank' rel='noreferrer' href='https://goo.gl/8V3jBy'> https://goo.gl/8V3jBy</a>)`,
+      date: "June-July 2022",
+      details: [
+        `Taught Students about  <strong>Science & Maths </strong> in a NGO<strong></strong> This ia part of our university social internship program.
+       This Internship Helped me in developing work experience & team <strong></strong> <strong></strong>`
+      ]
+    }
+  ],
+  certifications: [
+    {
+      desc: `<strong>AWS Fundamentals</strong> by <strong>Amazon Web Services</strong>.
+      (<a target='_blank' rel='noreferrer' href='https://www.coursera.org/account/accomplishments/specialization/ZQYU4AJB3P5E'>https://goo.gl/RGRzVn</a>)`,
+      date: "August 2022"
+    },
+    {
+      desc: `<strong>Security in Google Cloud</strong> by <strong>Google Cloud Training</strong> 
+       (<a target='_blank' rel='noreferrer' href='https://www.coursera.org/account/accomplishments/specialization/EME732C3ZCQ9'>
+       https://goo.gl/X3HEdR</a>)`,
+      date: "Feb 2023"
+    },
+    {
+      desc: `<strong>AWS Cloud Solutions Architect</strong> by <strong>Amazon Web Services</strong>.
+      (<a target='_blank' rel='noreferrer' href='https://www.coursera.org/account/accomplishments/professional-cert/N5F7UDDGLHGQ'>https://goo.gl/RGRzVn</a>)`,
+      date: "June 2023"
+    },
+    {
+      desc: `<strong>Architecting with Google Compute Engine</strong> by <strong>Google Cloud Training</strong>.
+      (<a target='_blank' rel='noreferrer' href='https://www.coursera.org/account/accomplishments/specialization/LUDP5CWFTUJP'>https://goo.gl/RGRzVn</a>)`,
+      date: "June 2023"
+    }
+  ],
+  events: []
+};
